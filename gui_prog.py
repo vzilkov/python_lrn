0b5d4ff0988dcc2810897af50db844232cd32e27
Vladislav Zilkov <vladislav.zilkov@gmail.com>
2020-03-06 11:40:23 +0300

    colored text in terminal (haven't tested)

--- gui_prog.py
+++ gui_prog.py
@@ -2,6 +2,8 @@ from tkinter import *
 
 #test
 import random
+from colorama import init 
+from termcolor import colored 
 
 root = Tk()
 root.geometry("1024x600")
@@ -9,17 +11,16 @@ root.resizable(FALSE, FALSE)
 
 check_btn_var = BooleanVar(value=FALSE)
 
-textbox = Text(root, heigh=30, width=80)
-textbox.tag_config('changed', background ='GREY', foreground='white')
-textbox.config(state='disabled')
+#textbox = Text(root, heigh=30, width=80)
+#textbox.tag_config('changed', background ='GREY', foreground='white')
+#textbox.config(state='disabled')
 #scroll_text = Scrollbar(orient=VERTICAL, command=textbox.yview)
 #textbox.configure(yscrollcommand=scroll_text.set)
 #scroll_text.pack(side="right", fill="y")
 
-fontstyle = ('sans-serif', 11)
-label_can_data_text = Label(root, text='  ID      Length      Data1      Data2      Data3       Data4      Data5      Data6       Data7       Data8', font=fontstyle)
-label_can_data_text.config(font=fontstyle)
-textbox.pack(side=LEFT)
+#fontstyle = ('sans-serif', 11)
+#label_can_data_text = Label(root, text='  ID      Length      Data1      Data2      Data3       Data4      Data5      Data6       Data7       Data8', font=fontstyle)
+#label_can_data_text.config(font=fontstyle)
 
 #buttons
 checkbutton_filters_masks = Checkbutton(root, 
@@ -41,9 +42,6 @@ entry_can_mask = Entry(root, text='enter values in hex 0-7FF')
 
 #places
 x_column_periph = 650
-
-textbox.place(x=0, y=20)
-label_can_data_text.place(x=0, y=0)
 
 checkbutton_filters_masks.place(x=x_column_periph, y=10)
 
@@ -70,37 +68,23 @@ def printing():
 
     global list_can_data_prev
 
-    textbox.config(state='normal')
-    column_text = 0
+    print('\nID\tLength\tData1\tData2\tData3\tData4\tData5\tData6\tData7\tData8')
+    #print(*list_can_data_prev, sep='\t')
+    #print([hex(x) for x in list_can_data_prev], sep='\t')
     for i in range(10):
-        textbox.insert(0.0, '%x\t' % (list_can_data[i]))
-        print(list_can_data, '\n')
-        column_text +=1
-    textbox.config(state='disabled')
-    #textbox.yview('end')
-    textbox.insert('2.0', '\n')
-    
-    count += 1
-    '''textbox.insert('0.0', '%x\t%x\t%x\t%x\t%x\t%x\t%x\t%x\t%x\t' % 
-                (list_can_data[1], list_can_data[2], list_can_data[3], list_can_data[4],
-                list_can_data[5], list_can_data[6], list_can_data[7], list_can_data[8], list_can_data[9]) + '\n')'''
-
-    if count == 255:
-        count = 0
+        print(colored(hex(list_can_data_prev[i]), 'green', 'on_red'), end='\t')
     #end test
-
 
     if check_btn_var.get() is False: #if checkbtn pressed
         entry_can_filter.config(state=DISABLED)
         entry_can_mask.config(state=DISABLED)
-        
     else:
         entry_can_filter.config(state=NORMAL)
         entry_can_mask.config(state=NORMAL)
     
         
-    root.after(50, printing)
+    root.after(80, printing)
 
 
-root.after(100, printing)
+root.after(400, printing)
 root.mainloop()
