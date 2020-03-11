705172e2815f302a03897bc689a2d9c3e58c7f54
Vladislav Zilkov <vladislav.zilkov@gmail.com>
2020-03-11 16:46:18 +0300

    edited gitignore, suppose dict is clear, trying to find some data

--- gui_prog.py
+++ gui_prog.py
@@ -7,6 +7,7 @@ from colorama import init
 from os import system, name 
 import pandas
 import os
+import tabulate
 
 root = Tk()
 root.geometry("1024x600")
@@ -60,20 +61,43 @@ checkbutton_trace.place(x=x_column_periph, y=130)
 
 count = 0
 list_can_data_prev = [0,1,2,3,4,5,6,7,8,9]
-dict_can_data = dict(id=0, lenght=0, data={0:10,1:11,2:12,3:13,4:14,5:15,6:16,7:17})
+#dict_can_data = dict(id=0, lenght=0, data=[0:10,1:11,2:12,3:13,4:14,5:15,6:16,7:17])
 
-print_can_data = [{'id':11, 'lenght':8, 'data':{0:1,1:2, 2:3, 3:4, 4:5, 5:6, 6:7,7:8}}]
+#print_can_data = [{'id':11, 'lenght':8, 'data':{0:21,1:22, 2:23, 3:24, 4:25, 5:26, 6:27,7:28}}]
+print_can_data = {0: {'id': 11, 'lenght': 8, 
+'data0': 21, 'data1': 22, 'data2': 23, 'data3': 24, 
+'data4': 25, 'data5': 26, 'data6': 27, 'data7': 28}}
+
 
 def printing():
     global count
     global check_btn_var, check_btn_var_light_up
     global list_can_data_prev
+    keys_vals = {'id','lenght',
+    'data0','data1','data2','data3'
+    'data4','data5','data6','data7'}
     #start test
     os.system('cls')
-    add_data = {'id':2, 'lenght':7, 'data':{0:11,1:12,2:13,3:14,4:15,5:16,6:17,7:18}}
-    print_can_data.append(add_data)
-    df = pandas.DataFrame(print_can_data)
-    print(df)
+
+    lenght = random.randrange(0,8)
+    can_id = random.randrange(0,0x7FF)
+    can_data = []
+    for i in range(8):
+        can_data.append(random.randrange(0, 0xFF))
+
+    add_data = {1:{'id': can_id, 'lenght': lenght}}
+    if lenght > 0:
+        for i in range(lenght):
+            add_data[1]['data%d' % (i)] = can_data[i]
+    
+    print(add_data)
+
+    for i in range(print_can_data[0]['lenght']):
+        print_can_data[0]['data'+str(i)] = random.randrange(0, 20)
+    print(print_can_data[0])
+    #df = pandas.DataFrame(print_can_data)
+    #print(df.to_markdown())
+    #df.to_excel("output.xlsx")
     '''list_can_data = [1, 2, random.randrange(0,0xFF), random.randrange(0,0xFF), random.randrange(0,0xFF),
                      random.randrange(0,0xFF), random.randrange(0,0xFF), random.randrange(0,0xFF), random.randrange(0,0xFF), random.randrange(0,0xFF)]
 
