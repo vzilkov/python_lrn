81f3891b7e4976914d3808798ac531c5315e62bf
abledev <vladislav.zilkov@gmail.com>
2020-09-14 17:04:16 +0300

    haven't tested yet, added class

--- simple_server.py
+++ simple_server.py
@@ -2,10 +2,14 @@
 
 import socket
 
+class eth_class:
+ def __init__(self, ip, port):
+  print('IP: ', ip, ' port=%d'%(port))
+
 TCP_IP = ''
 TCP_PORT = 5560
 BUFFER_SIZE = 1024  # Normally 1024, but we want fast response
-
+x = eth_class('192.168.2.55',TCP_PORT)
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.bind((TCP_IP, TCP_PORT))
