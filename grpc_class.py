25f4d11a1c8ce70ba1649be83fb2881fae614c74
Vladislav Zilkov <vladislav.zilkov@gmail.com>
2020-10-06 17:04:01 +0300

    added to previous commit

--- grpc_class.py
+++ grpc_class.py
@@ -1,7 +1,7 @@
 import socket
 import logging
-UDP_IP = ""
-UDP_PORT = 55001
+#UDP_IP = ""
+#UDP_PORT = 55001
 
 '''logging.basicConfig(level=logging.INFO,
                     #filename=file_name,
@@ -15,27 +15,34 @@ import grpc
 import CanService_pb2
 import CanService_pb2_grpc
 from google.protobuf import empty_pb2
+
+import threading
+import sys
+
 channel = grpc.insecure_channel(('%s:%d'%(TCP_IP, 50051)))
 stub = CanService_pb2_grpc.CanOperationsStub(channel)
 
-class grpc_response_stream_rcv():
+import queue
+can_data_queue = queue.Queue()
+
+class grpc_response_stream_rcv(threading.Thread):
     def __init__(self):
-        self.count = count = 1
+        self.count = 1
+        self.receive_stream_enable = True
         self.response = stub.StartMessagesStream(empty_pb2.Empty())
 
-    def receive(self):
-        for i in self.response:
-            #string_buf = " ".join(["{:02x}".format(x) for x in (i.Data)])
-            print('Count = %d, Received from server: %X, Len %d, Data = ' % (self.count, i.Id, i.Len), [j for j in i.Data])
-            self.count += 1
+    def receive_stream(self):
+        try:
+            for i in self.response:
+                print('Count = %d, Received from server: %X, Len %d, Data = ' % (self.count, i.Id, i.Len), [j for j in i.Data])
+                self.count += 1
+                can_data_queue.put(i)
+        except:
+            print('Class grpc_response_stream_rcv EXCEPT')
 
-class_grpc = grpc_response_stream_rcv()
-class_grpc.receive()
+    def start_receive_stream(self):
+        self.producer = threading.Thread(target=self.receive_stream)
+        self.producer.start()
 
-'''
-response = stub.StartMessagesStream(empty_pb2.Empty())
-        for i in response:
-            #string_buf = " ".join(["{:02x}".format(x) for x in (i.Data)])
-            print('Count = %d, Received from server: %X, Len %d, Data = ' % (count, i.Id, i.Len), [j for j in i.Data])
-            count += 1
-'''
+    def stop_thread(self):
+        self.response.cancel()
--- test.py
+++ test.py
@@ -1,68 +1,84 @@
 from PyQt5.uic import loadUi
-from PyQt5 import QtWidgets, QtCore
+from PyQt5 import QtWidgets, QtCore, QtGui
 from PyQt5.QtWidgets import QApplication, QDialog
 import sys
+import threading
+
+from grpc_class import *
+import queue
+
+#can_data_queue = queue.Queue()
 
 class MainWindow(QDialog):
     def __init__(self):
-        super(MainWindow,self).__init__()
+        super(MainWindow, self).__init__()
         loadUi('main.ui', self)
-        
-    def loaddata(self):
-        self.tableWidget.resizeColumnsToContents()
-        from random import randrange
-        for i in range(0, 100, 1):
-            row = mainwindow.tableWidget.rowCount()
-            self.tableWidget.insertRow(row)
-            self.tableWidget.setItem(row, 0, QtWidgets.QTableWidgetItem('%X'% randrange(0x200)))
-            self.tableWidget.setItem(row, 1, QtWidgets.QTableWidgetItem('%X'% randrange(255)))
-            self.tableWidget.setItem(row, 2, QtWidgets.QTableWidgetItem('%X'% randrange(255)))
-            self.tableWidget.setItem(row, 3, QtWidgets.QTableWidgetItem('%X'% randrange(255)))
-            self.tableWidget.setItem(row, 4, QtWidgets.QTableWidgetItem('%X'% randrange(255)))
-            self.tableWidget.setItem(row, 5, QtWidgets.QTableWidgetItem('%X'% randrange(255)))
-            self.tableWidget.setItem(row, 6, QtWidgets.QTableWidgetItem('%X'% randrange(255)))
-            self.tableWidget.setItem(row, 7, QtWidgets.QTableWidgetItem('%X'% randrange(255)))
-            self.tableWidget.setItem(row, 8, QtWidgets.QTableWidgetItem('%X'% randrange(255)))
-            self.tableWidget.setItem(row, 9, QtWidgets.QTableWidgetItem('%X'% randrange(255)))
-            self.tableWidget.setItem(row, 10, QtWidgets.QTableWidgetItem('%X'% row))
+        self._scroll = True
+        #self.Qt
+        self.rcv_data_func_en = True
+        self.cnt = 0
 
+    def tx_data_func(self):
+        can_data = []
+        can_data[0] = self.lineEdit_data_0.text()
+        can_data[1] = self.lineEdit_data_1.text()
+        can_data[2] = self.lineEdit_data_2.text()
+        can_data[3] = self.lineEdit_data_3.text()
+        can_data[4] = self.lineEdit_data_4.text()
+        can_data[5] = self.lineEdit_data_5.text()
+        can_data[6] = self.lineEdit_data_6.text()
+        can_data[7] = self.lineEdit_data_7.text()
+        ID = self.lineEdit_id.text()
+        self.comboBox_len.text()
+
+    def rcv_data_func(self):
+        while self.rcv_data_func_en:
+            self.tx_data_func()
+            #data
+            try:
+                data = can_data_queue.get(timeout=2)
+            except:
+                print('Queue timeout occured')
+            #row = mainwindow.tableWidget.rowCount()
+            if data:
+                row = 0
+                self.tableWidget.insertRow(row)
+                self.tableWidget.setItem(row, 0, QtWidgets.QTableWidgetItem('%X'% data.Id))
+                self.tableWidget.setItem(row, 1, QtWidgets.QTableWidgetItem('%X'% data.Len))
+
+                count = 0
+                for i in data.Data:
+                   self.tableWidget.setItem(row, count+2, QtWidgets.QTableWidgetItem('%X'% i))
+                   count += 1
+                
+                self.tableWidget.setItem(row, 10, QtWidgets.QTableWidgetItem('%d'% self.cnt))
+                self.tableWidget.setItem(row, 11, QtWidgets.QTableWidgetItem('RX'))
+
+                self.tableWidget.resizeColumnsToContents()
+                
+                self.cnt += 1
 
 app = QApplication(sys.argv)
 mainwindow = MainWindow()
 widget = QtWidgets.QStackedWidget()
 widget.addWidget(mainwindow)
 
-#for i in range(0,10, 1):
-#    mainwindow.tableWidget.setColumnWidth(i,75)
-mainwindow.tableWidget.setFixedWidth(680)
-mainwindow.loaddata()
-    
+class_grpc = grpc_response_stream_rcv()
+
+#mainwindow.tableWidget.setFixedWidth(680)
 widget.show()
+
+class_grpc.start_receive_stream()
+
+cons = threading.Thread(target = mainwindow.rcv_data_func)
+cons.start()
+
 try:
+    mainwindow.tx_data_func()
     sys.exit(app.exec_())
 except:
     print('Exiting')
-'''
-class Ui(QtWidgets.QDialog, form):
-    def __init__(self):
-        super(Ui, self).__init__()
-        self.setupUi(self)
-        self.button1.clicked.connect(self.print_btn_pressed)
-    
-    def print_btn_pressed(self):
-        if(self.checkBox.checkState()):
-            self.rcv_can_msgs.append("ChkBtn true\n")
-        else:
-            #cursor = self.textCursor()
-            #cursor.insertText('Cursor enters text')
-            #cursor.movePosition(QtGui)
-            #print('Anchor position %d'%self.rcv_can_msgs.setTextCursor(0))
-            self.rcv_can_msgs.insertPlainText("ChkBtn false\n")
-
-    def checkBox_pressed(self, txt):
-        print('%s' % txt)
-
-app = QtWidgets.QApplication([])
-main_window = Ui()
-main_window.show()
-sys.exit(app.exec())'''
+finally:
+    class_grpc.stop_thread()
+    mainwindow.rcv_data_func_en = False
+    print('finally')
